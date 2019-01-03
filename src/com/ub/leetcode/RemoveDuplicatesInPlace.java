d21c8cfc0b57eeec588173de02d9e25d49094846
Apurba Mahanta <apurba5964@gmail.com>
2019-01-03 15:37:51 -0500

    Added removeElement

--- src/com/ub/leetcode/RemoveDuplicatesInPlace.java
+++ src/com/ub/leetcode/RemoveDuplicatesInPlace.java
@@ -1,5 +1,6 @@
 /*
 https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+https://leetcode.com/problems/remove-element/
 */
 package com.ub.leetcode;
 
@@ -7,6 +8,10 @@ public class RemoveDuplicatesInPlace {
 
 	public static void main(String[] args) {
 		
+		int[] input = {0,1,2,2,3,0,4,2};
+		System.out.println(removeElement(input, 2));
+		
+				
 	}
 	
 	
@@ -16,16 +21,39 @@ public static int removeDuplicates(int[] nums) {
 		return 0;
 	
 	int i=0;
-	for(int j=1;j<nums.length;j++)
+	for(int j=1;j<nums.length;j++) {
 	
 		if(nums[i]!=nums[j]) {
 			i++;
 			nums[i]=nums[j];
 		}
 		
-	
+	}
        return i+1;
     }
+
+
+public static int removeElement(int[] nums, int val) {
+	if(nums.length==0)
+		return 0;
 	
+	int i=0;
+	for(int j=0;j<nums.length;j++) {
+		
+		if(nums[j]!=val) {
+			 {
+				//i++;	
+			nums[i]=nums[j];
+			i++;
+			}
+			
+		}
+		
+	}
+	
+	
+	return i;
+    
+}
 	
 }
