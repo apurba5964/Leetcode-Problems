ef80707dae633c322ad834fb3045deffe4498172
Apurba Mahanta <apurba5964@gmail.com>
2019-02-04 21:17:28 -0500

    https://leetcode.com/problems/missing-number/solution/
    https://leetcode.com/problems/missing-number/solution/

--- src/com/ub/leetcode/easy/ValidPalindrome.java
+++ src/com/ub/leetcode/easy/ValidPalindrome.java
@@ -1,8 +1,12 @@
 /*
 https://leetcode.com/problems/valid-palindrome/
 https://leetcode.com/problems/single-number/
+https://leetcode.com/problems/missing-number/
 */
 package com.ub.leetcode.easy;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class ValidPalindrome {
 
@@ -11,7 +15,9 @@ public class ValidPalindrome {
 		
 		System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
 		int[] input = {4,1,2,1,2};
+		int[] input1 = {9,6,4,2,3,5,7,0,1};
 		System.out.println(singleNumber(input));
+		System.out.println(missingNumber(input1));
 	}
 
 	public static boolean isPalindrome(String s) {
@@ -41,6 +47,29 @@ public class ValidPalindrome {
 		 
 		 
 		return result;
+	        
+	    }
+	 
+	 public static int missingNumber(int[] nums) {Arrays.sort(nums);
+		if (nums[nums.length-1] != nums.length) {
+            return nums.length;
+        }
+        else if (nums[0] != 0) {
+            return 0;
+        }
+
+		
+		 int index=0;
+		 
+		 for(int i=nums.length-1;i>0;i--) {
+			 System.out.println(nums[i]);
+			 if(nums[i]-nums[i-1]!=1) {
+				 index=i;
+				 break;
+				 }
+		 }
+		 
+		return nums[index]-1;
 	        
 	    }
 
