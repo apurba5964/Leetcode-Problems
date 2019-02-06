f2bd07ab000b34e700a82025ad3e338df1cdf278
Apurba Mahanta <apurba5964@gmail.com>
2019-02-05 20:13:34 -0500

    https://leetcode.com/problems/linked-list-cycle/submissions/
    https://leetcode.com/problems/linked-list-cycle/submissions/

--- src/com/ub/leetcode/easy/ValidPalindrome.java
+++ src/com/ub/leetcode/easy/ValidPalindrome.java
@@ -2,6 +2,7 @@
 https://leetcode.com/problems/valid-palindrome/
 https://leetcode.com/problems/single-number/
 https://leetcode.com/problems/missing-number/
+https://leetcode.com/problems/linked-list-cycle/
 */
 package com.ub.leetcode.easy;
 
@@ -10,67 +11,87 @@ import java.util.HashSet;
 
 public class ValidPalindrome {
 
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
 	public static void main(String[] args) {
 
-		
 		System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
-		int[] input = {4,1,2,1,2};
-		int[] input1 = {9,6,4,2,3,5,7,0,1};
+		int[] input = { 4, 1, 2, 1, 2 };
+		int[] input1 = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
 		System.out.println(singleNumber(input));
 		System.out.println(missingNumber(input1));
 	}
 
 	public static boolean isPalindrome(String s) {
-		if(s.isEmpty())
+		if (s.isEmpty())
 			return true;
-		
-		s=s.trim().toLowerCase().replaceAll("\\s+", "").replaceAll("[^A-Za-z0-9]", "");;
+
+		s = s.trim().toLowerCase().replaceAll("\\s+", "").replaceAll("[^A-Za-z0-9]", "");
+		;
 		StringBuilder sbuild = new StringBuilder(s);
-		
-		String temp=sbuild.reverse().toString().toLowerCase();
-		
-		
-		if(temp.equalsIgnoreCase(s))
+
+		String temp = sbuild.reverse().toString().toLowerCase();
+
+		if (temp.equalsIgnoreCase(s))
 			return true;
 		else
 			return false;
 
 	}
-	
-	 public static int singleNumber(int[] nums) {
-		
-		 int result = 0;
-		 for(int i=0;i<nums.length;i++) {
-		 
-			 result = result ^ nums[i];
-		 }
-		 
-		 
-		return result;
-	        
-	    }
-	 
-	 public static int missingNumber(int[] nums) {Arrays.sort(nums);
-		if (nums[nums.length-1] != nums.length) {
-            return nums.length;
-        }
-        else if (nums[0] != 0) {
-            return 0;
-        }
 
-		
-		 int index=0;
-		 
-		 for(int i=nums.length-1;i>0;i--) {
-			 System.out.println(nums[i]);
-			 if(nums[i]-nums[i-1]!=1) {
-				 index=i;
-				 break;
-				 }
-		 }
-		 
-		return nums[index]-1;
-	        
-	    }
+	public static int singleNumber(int[] nums) {
+
+		int result = 0;
+		for (int i = 0; i < nums.length; i++) {
+
+			result = result ^ nums[i];
+		}
+
+		return result;
+
+	}
+
+	public static int missingNumber(int[] nums) {
+		Arrays.sort(nums);
+		if (nums[nums.length - 1] != nums.length) {
+			return nums.length;
+		} else if (nums[0] != 0) {
+			return 0;
+		}
+
+		int index = 0;
+
+		for (int i = nums.length - 1; i > 0; i--) {
+			System.out.println(nums[i]);
+			if (nums[i] - nums[i - 1] != 1) {
+				index = i;
+				break;
+			}
+		}
+
+		return nums[index] - 1;
+
+	}
+
+	public boolean hasCycle(ListNode head) {
+		if (head == null || head.next == null)
+			return false;
+		ListNode fast = head, slow = head;
+		while (fast != null && fast.next != null && slow != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+			if (fast == slow)
+				return true;
+		}
+		return false;
+	}
 
 }
