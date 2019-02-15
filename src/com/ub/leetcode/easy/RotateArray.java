f6b819ed6367886cbec27b46b0ee3fc4c11dea02
Apurba Mahanta <apurba5964@gmail.com>
2019-02-15 18:00:35 -0500

    https://leetcode.com/problems/number-of-1-bits/solution/
    https://leetcode.com/problems/number-of-1-bits/solution/

--- src/com/ub/leetcode/easy/RotateArray.java
+++ src/com/ub/leetcode/easy/RotateArray.java
@@ -1,5 +1,7 @@
-/*https://leetcode.com/problems/rotate-array/
-
+/*
+https://leetcode.com/problems/rotate-array/
+https://leetcode.com/problems/reverse-bits/
+https://leetcode.com/problems/number-of-1-bits/solution/
 */
 
 package com.ub.leetcode.easy;
@@ -7,6 +9,7 @@ package com.ub.leetcode.easy;
 public class RotateArray {
 	
 	public static void main(String[] args) {
+		
 		
 	}
 	
@@ -31,6 +34,32 @@ public class RotateArray {
 		}
 		
 		
+    }
+	
+	public static int reverseBits(int n) {
+		
+		String s = Integer.toBinaryString(n);
+		StringBuilder sb = new StringBuilder(s);
+		String reverse = sb.reverse().toString();
+		for(int i = 0; i < 32-s.length(); i++)
+			reverse= reverse + "0";
+    	
+		
+		
+        return Integer.valueOf(reverse);
+    }
+	
+   public int hammingWeight(int n) {
+	   int bits = 0;
+	   int mask = 1;
+	   for(int i=0;i<32;i++) {
+		   if((n & mask)!=0)
+			   bits++;
+		   mask = mask<<1;
+	   }
+	   
+	return bits;
+        
     }
 
 }
