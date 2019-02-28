3fbf63b0b085f26a9ca2b99fc872db0fab30baf2
Apurba Mahanta <apurba5964@gmail.com>
2019-02-28 03:26:31 -0500

    https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
    https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/

--- src/com/ub/leetcode/medium/CombinationSum.java
+++ src/com/ub/leetcode/medium/CombinationSum.java
@@ -1,6 +1,6 @@
 /*
 https://leetcode.com/problems/combination-sum-ii/
-
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 */
 package com.ub.leetcode.medium;
 
@@ -48,5 +48,39 @@ public class CombinationSum {
 		}
 		
 	}
+	
+	
+	public int[] searchRange(int[] nums, int target) {
+        
+		  
+		   int[] result = {-1,-1};
+		   int h = nums.length;
+		   int lindex = mergeSort(nums,target,0,h,true);
+		   
+	        if (lindex == nums.length || nums[lindex] != target) {
+	            return result;
+	        }
+	        result[0] = lindex;
+	        result[1] =  mergeSort(nums,target,0,h,false)-1;
+	        
+	        return result;
+	    
+	    }
+	    
+	    private int mergeSort(int[] nums, int target, int low, int high,boolean check) {
+		
+		
+		
+		while(high>low) {   
+		int mid = (low+high)/2;
+		if(nums[mid] > target || (check && target == nums[mid]))
+			high  = mid;
+	    else
+	        low = mid + 1;
+		
+		}
+		return low;
+	    
+	    }
 
 }
