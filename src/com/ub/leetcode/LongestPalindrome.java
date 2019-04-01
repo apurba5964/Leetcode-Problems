5e4dd62216202e8811b633e472067fe41df51ad0
Apurba Mahanta <apurba5964@gmail.com>
2019-03-31 22:05:17 -0400

    Pow Implementation
    Pow Implementation

--- src/com/ub/leetcode/LongestPalindrome.java
+++ src/com/ub/leetcode/LongestPalindrome.java
@@ -3,16 +3,18 @@ package com.ub.leetcode;
 public class LongestPalindrome {
 	
 	public static void main(String[] args) {
-		System.out.println(count_palindromes("hellolle"));
+		/*System.out.println(count_palindromes("hellolle"));
 		
 		int[] input={ 2, 3, 4, 10, 40 };
 		System.out.println(input.length);
 		for (int i : input) {
 			System.out.print(i+"\t");
-		}
+		}*/
+		
+		System.out.println(longestPalindrome1("geekeg"));
 	}
 	
-	 public String longestPalindrome(String s) {
+	 public static String longestPalindrome(String s) {
 		 
 		 int n=s.length();
 		 
@@ -49,12 +51,50 @@ public class LongestPalindrome {
 				 
 			 }
 		 }
-		 
+		 System.out.println();
 		 
 		return s.substring(start, start+max);
 	     
 		 
 	    }
+	 
+	 public static String longestPalindrome1(String s) {
+	
+		 int n = s.length();
+		 int len =1;
+		 int start = 0;
+		 boolean[][] checkTable = new boolean[n][n];
+		 
+		 for(int i =0;i<n;i++) {
+			 checkTable[i][i]=true;
+		 }
+		 
+		 for(int i=0;i<n-1;i++) {
+			if(s.charAt(i)==s.charAt(i+1)) {
+				checkTable[i][i+1]=true;
+				len =2;
+			}else {
+				checkTable[i][i+1]=false;
+			 }
+		 }
+		 
+		 for(int k = 3;k<=n;k++) {
+			 for(int i=0;i<n-k+1;i++) {
+				 int j = i+k-1;
+				 if(s.charAt(i)==s.charAt(j)) {
+					 if(checkTable[i+1][j-1]) {
+						 checkTable[i][j]=true;
+						 len = k;
+						 start = i;
+					 }
+				 }
+			 }
+		 }
+		 
+		 
+		 System.out.println(len);
+		 return s.substring(start, start+len);
+	 }
 	 
 	 
 	 public static int count_palindromes(String s) {
