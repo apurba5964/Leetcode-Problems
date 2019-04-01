5b887fa53b0b15351a45c911e8bd434ac69132b2
Apurba Mahanta <apurba5964@gmail.com>
2019-04-01 01:22:59 -0400

    counting-inversions
    counting-inversions

--- src/com/ub/leetcode/medium/Pow.java
+++ src/com/ub/leetcode/medium/Pow.java
@@ -1,13 +1,17 @@
 
 /*
 https://leetcode.com/problems/powx-n/
+
+https://www.geeksforgeeks.org/counting-inversions/
 */
 package com.ub.leetcode.medium;
 
 public class Pow {
 
 	public static void main(String[] args) {
-		System.out.println(pow(2,-1));
+		//System.out.println(pow(2,-1));
+		int arr[] = new int[] { 1, 20, 6, 4, 5 };
+		System.out.println(countInversions(arr));
 	}
 	
 	public static double pow(double x, int m) {
@@ -29,4 +33,64 @@ public class Pow {
         }
 	
 }
+	
+	
+	public static int countInversions(int[] arr) {
+		
+		int n = arr.length;
+		int l = 0;
+		
+		int[] temp = new int[arr.length];
+		int count = mergeSort(arr,temp,l,n-1);
+		return count;
+	}
+
+	private static int mergeSort(int[] arr, int[] temp, int l, int r) {
+		int inv_cnt = 0;
+		
+		if(r>l) {
+		int mid = (l+r)/2;
+		
+		inv_cnt = mergeSort(arr, temp, l, mid);
+		inv_cnt += mergeSort(arr, temp, mid+1, r);
+		
+		
+		inv_cnt += merge(arr,temp,l,mid+1,r);
+		
+		}
+		
+		
+		return inv_cnt;
+	}
+
+	private static int merge(int[] arr, int[] temp, int l, int mid, int r) {
+		int inv_cnt =0;
+		int i = l;
+		int j =mid;
+		int k=l;
+		while(i<mid && j<=r) {
+			if(arr[i]<arr[j]) {
+				temp[k++] = arr[i++];
+				
+			}else {
+				inv_cnt += mid-i;
+				temp[k++] = arr[j++];
+				
+			}
+			
+		}
+		while(i<mid) {
+			temp[k++]=arr[i++];
+		}
+		while(j<=r) {
+			temp[k++]=arr[j++];
+		}
+		
+		for (i = l; i <= r; i++) 
+            arr[i] = temp[i];
+		
+		
+		return inv_cnt;
+	}
+	
 }
