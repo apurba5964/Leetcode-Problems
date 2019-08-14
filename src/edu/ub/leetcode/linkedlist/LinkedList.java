0df3e0c5a0d5c4ea7a451bd839b26ac739e2a8c0
Apurba Mahanta <apurba5964@gmail.com>
2019-08-13 21:58:44 -0400

    LRU implementation

--- src/edu/ub/leetcode/linkedlist/LinkedList.java
+++ src/edu/ub/leetcode/linkedlist/LinkedList.java
@@ -4,7 +4,7 @@
 	
 package edu.ub.leetcode.linkedlist;
 
-
+import java.util.HashMap;
 
 public class LinkedList {
 	
@@ -78,3 +78,116 @@ public class LinkedList {
 	
 	
 }
+
+
+
+
+class LRUCache {
+    int cap=0,curr_cap=0;
+    HashMap<Integer,ListNode> map;
+    
+     class ListNode {
+		 int key;
+         int val;
+		 ListNode left;
+         ListNode right;
+
+		ListNode(int x,int y) {
+			key = x;val=y;
+		}
+    }
+    ListNode head,tail;
+    public LRUCache(int capacity) {
+        cap=capacity;
+        map = new HashMap<>();
+        head = new ListNode(-1,-1);
+        tail = new ListNode(-1,-1);
+    }
+    
+    public int get(int key) {
+        if(map.get(key)==null)
+            return -1;
+        else{
+            ListNode node = map.get(key);
+            deleteNode(node);
+            addNodeToTail(node);
+            return node.val;
+        }
+            
+    }
+    
+    public void put(int key, int value) {
+       
+        ListNode node = new ListNode(key,value);
+        if(curr_cap==0){
+            
+            head.right=node;
+            tail.left=node;
+            node.right=tail;
+            node.left=head;
+            map.put(node.key,node);
+            curr_cap++;
+            return;
+        }
+        
+        if(curr_cap<cap){
+        if(map.get(key)==null){
+             addNodeToTail(node);
+             
+        }else{
+            ListNode node1 = map.get(key);
+            deleteNode(node1);
+            addNodeToTail(node);
+        }
+        }else{
+            if(map.get(key)!=null){
+            ListNode node1 = map.get(key);
+            deleteNode(node1);
+            addNodeToTail(node);
+         }else{
+            deleteNodeFromHead();
+            addNodeToTail(node);
+            }
+            
+        }
+     
+        // System.out.println(map.size());
+    }
+    
+    public void addNodeToTail(ListNode node){
+           ListNode left = tail.left;
+           left.right=node;
+           node.left=left;
+           node.right=tail;
+           tail.left=node;
+           curr_cap++;
+           map.put(node.key,node);
+    }
+    
+    public void deleteNode(ListNode node){
+           ListNode left = node.left;
+           ListNode right = node.right;
+           left.right=right;
+           right.left=left;
+           
+           map.remove(node.key);
+           curr_cap--;
+          // node = null;
+    }
+    
+    public void deleteNodeFromHead(){
+        ListNode node = head.right;
+        head.right = node.right;
+        node.right.left=head;
+        map.remove(node.key);
+        curr_cap--;
+        node=null;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
